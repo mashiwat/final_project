a0eaa7c3681ac7416885d3a001184765185a4fba
Your Name <you@example.com>
2021-03-12 14:30:24 -0600

    added requests received

--- myswaps.js
+++ myswaps.js
@@ -24,7 +24,7 @@ firebase.auth().onAuthStateChanged(async function(user) {
     
       let response = await db.collection('interested').get()
      let interested = response.docs
-      for (i = 0; i <interested.length; i++){
+      for (i = 0; i <interested.length; i++) {
           let interestedId = interested[i].id
           let interestedData = interested[i].data()
           let interestedProduct = interestedData.productName
@@ -32,6 +32,8 @@ firebase.auth().onAuthStateChanged(async function(user) {
           let interestedPrice = interestedData.productPrice
           let interesteduser = interestedData.userId
           let interestedowner = interestedData.owneremail
+          let interestedrequestor = interestedData.requestoremail
+          
           if (interesteduser == user.uid){
             document.querySelector('.userspecificrequests').insertAdjacentHTML('beforeend', `
             <div class="bg-white rounded-xl border-2 border-blue-400 pb-4">
@@ -41,6 +43,16 @@ firebase.auth().onAuthStateChanged(async function(user) {
             <p class="block px-6 py-2">Requested from: ${interestedowner}</p>
             </div>`)
           }
+          if (interestedowner == user.email){
+            document.querySelector('.requestsreceived').insertAdjacentHTML('beforeend', `
+            <div class="bg-white rounded-xl border-2 border-blue-400 pb-4">
+            <img src="${interestedImage}" class="block p-4">
+            <p class="block px-6 py-2">${interestedProduct}</p>
+            <p class="block px-6 py-2">${interestedPrice}</p>
+            <p class="block px-6 py-2">Requested by: ${interestedrequestor}</p>
+            </div>`)
+          }
+         
       }  
 
     } else {
