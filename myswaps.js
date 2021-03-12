93921cbe87101312f99a50cf326243df49066314
Your Name <you@example.com>
2021-03-12 10:15:52 -0600

    hide forms when logged out

---
+++ myswaps.js
@@ -0,0 +1,44 @@
+let db = firebase.firestore()
+
+firebase.auth().onAuthStateChanged(async function(user) {
+    if (user) {
+      // Signed in
+      console.log('signed in')
+      document.querySelector('.sign-in-or-sign-out').innerHTML = `
+        <button class="text-blue-500 underline sign-out">Sign Out</button>
+      `
+      document.querySelector('.sign-out').addEventListener('click', function(event) {
+        console.log('sign out clicked')
+        firebase.auth().signOut()
+        document.location.href = 'browse.html'
+      })
+
+      db.collection('users').doc(user.uid).set({
+        name: user.displayName,
+        email: user.email
+      })
+
+      document.querySelector('.hello').insertAdjacentHTML('beforeend', `
+      <div class="block">Hello, ${user.displayName}</div>
+      `)
+    } else {
+      // Signed out
+      console.log('signed out')
+      //Hide browse capabilties
+      document.querySelector
+  
+      // Initializes FirebaseUI Auth
+      let ui = new firebaseui.auth.AuthUI(firebase.auth())
+  
+      // FirebaseUI configuration
+      let authUIConfig = {
+        signInOptions: [
+          firebase.auth.EmailAuthProvider.PROVIDER_ID
+        ],
+        signInSuccessUrl: 'myswaps.html'
+      }
+  
+      // Starts FirebaseUI Auth
+      ui.start('.sign-in-or-sign-out', authUIConfig)
+    }
+})
